35a2c7fdf84d75ded10937f9d696de2a8ba42b02
Yash Kashiv <yashkashiv001@gmail.com>
2024-02-25 01:08:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder){
     currFolder = folder;
-    let a = await fetch(`transcendent-hummingbird-9c3efe/songs/${folder}`)
+    let a = await fetch(`/songs/${folder}`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML=response;
